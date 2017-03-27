f319002ec39d1f922e649a09fa8ebc05a1ca42d6
CisinoJr <cisinojr@gmail.com>
2017-03-27 00:56:09 -0300

    testing cep api

--- js/help.js
+++ js/help.js
@@ -3,7 +3,7 @@ jQuery(function($){
    $("#cep").change(function(){
       var cep_code = $(this).val();
       if( cep_code.length <= 0 ) return;
-      $.get("http://apps.widenet.com.br/busca-cep/api/cep.json", { code: cep_code },
+      $.get("https://apps.widenet.com.br/busca-cep/api/cep.json", { code: cep_code },
          function(result){
             if( result.status!=1 ){
                alert(result.message || "Houve um erro desconhecido");
